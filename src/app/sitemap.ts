a3271b8bc00601ce907ed2a5ac6fc6170df863f0
usamimi39 <usamin04tp@gmail.com>
2026-04-15 13:45:32 +0900

    set-githubpages-setting

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,27 +1,29 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
+
+export const dynamic = "force-static";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   // Cloudflareで設定した「wwwあり」のドメインを指定してください
-  const baseUrl = 'https://www.soundcreate.org' 
+  const baseUrl = "https://www.soundcreate.org";
 
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: `${baseUrl}/member`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: `${baseUrl}/release`,
       lastModified: new Date(),
-      changeFrequency: 'weekly', // 更新頻度が高いならweekly、低ければmonthlyでOK
+      changeFrequency: "weekly", // 更新頻度が高いならweekly、低ければmonthlyでOK
       priority: 0.8,
     },
-  ]
+  ];
 }
